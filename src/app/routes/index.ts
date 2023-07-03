a3cb083052712e81cbd1c31b9924541b28501a09
ashiqcsebu <ashiq.cse3.bu@gmail.com>
2023-07-03 19:41:11 +0600

    router created

---
+++ src/app/routes/index.ts
@@ -0,0 +1,34 @@
+import express from 'express';
+import { CowRoutes } from '../modules/cow/cow.route';
+import { OrderRoutes } from '../modules/order/order.route';
+import { UserRoutes } from '../modules/user/user.route';
+import { AdminRoutes } from '../modules/admin/admin.routes';
+import { AuthRoutes } from '../modules/auth/auth.route';
+
+const router = express.Router();
+
+const moduleRoutes = [
+  {
+    path: '/',
+    route: UserRoutes,
+  },
+  {
+    path: '/cows',
+    route: CowRoutes,
+  },
+  {
+    path: '/orders',
+    route: OrderRoutes,
+  },
+  {
+    path: '/admins',
+    route: AdminRoutes,
+  },
+  {
+    path: '/auth',
+    route: AuthRoutes,
+  },
+];
+
+moduleRoutes.forEach(route => router.use(route.path, route.route));
+export default router;
